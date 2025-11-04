a3a2772f7a2ef1044d414de20b65b3bae2cc66cc
Anders Christiansen Sørby <anders@sorby.xyz>
2025-11-04 21:07:29 +0100

    Add paging implementation

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,6 +1,8 @@
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{
+    InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode
+};
 use crate::println;
-
+use crate::hlt_loop;
 use lazy_static::lazy_static;
 use crate::gdt;
 use pic8259::ChainedPics;
@@ -40,6 +42,7 @@ lazy_static! {
 
         idt[InterruptIndex::Keyboard.as_u8()]
             .set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
 
         idt
     };
@@ -47,6 +50,19 @@ lazy_static! {
 
 pub fn init_idt() {
     IDT.load();
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(kodios::test_runner)]
 
-
+use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
-use kodios::hlt_loop;
+use kodios::{hlt_loop, memory::translate_addr, serial_println};
+use x86_64::VirtAddr;
+use x86_64::structures::paging::PageTable;
 #[cfg(not(test))]
 use kodios::println;
 
@@ -23,13 +25,33 @@ fn panic(info: &PanicInfo) -> ! {
     hlt_loop();
 }
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     kodios::init();
-    
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        //boot_info.physical_memory_offset, will fail
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        serial_println!("{:?} -> {:?}", virt, phys);
+    }
+
     #[cfg(test)]
     test_main();
 
     hlt_loop();
 }
-
