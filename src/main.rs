a5615cd9eb887e8cf5366e5bb711621fb7478e98
Anders Christiansen Sørby <anders@sorby.xyz>
2025-11-06 14:58:01 +0100

    Add basic frame allocator based on BootInfo

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,11 @@ use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
 #[cfg(not(test))]
 use kodios::println;
-use kodios::{hlt_loop, memory::translate_addr, serial_println};
-use x86_64::{VirtAddr, structures::paging::PageTable};
+use kodios::{
+    hlt_loop,
+    memory::{self, BootInfoFrameAllocator},
+};
+use x86_64::{VirtAddr, structures::paging::Page};
 
 #[cfg(test)]
 #[panic_handler]
@@ -29,25 +32,19 @@ entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     kodios::init();
 
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    // let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
 
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        //boot_info.physical_memory_offset, will fail
-    ];
+    //let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
-        serial_println!("{:?} -> {:?}", virt, phys);
-    }
+    // map an unused page
+    let page: Page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
